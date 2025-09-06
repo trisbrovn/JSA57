62b1f59120af3e1c43c8a3f91d7c8530842a28f7
trisbrovn <trisbrodz@gmail.com>
2025-09-06 11:57:54 +0700

    l2

--- L2/js/bai1.js
+++ L2/js/bai1.js
@@ -1,1 +1,18 @@
-// khaia
+// Khai báo biến city bằng từ khóa var với giá trị là chuỗi "Hà Nội".
+var city = "Ha Noi";
+// Khai báo biến age bằng từ khóa let với giá trị là số 25.
+let age = 25;
+// Khai báo biến PI bằng từ khóa const với giá trị là số 3.14.
+const PI = 3.14;
+// Khai báo các biến temperature, greeting, isJavaScriptFun, nothing, notDefined, uniqueID, numbers, và person với các giá trị thích hợp.
+var temperature = 30;
+var greeting = "Hello world!";
+var isJavaScriptFun = true;
+var nothing = null;
+var notDefined = undefined;
+const uniqueID = 1;
+const numbers = [123,45.7,45]
+const person = {id: 755, firstName: "ABC", lastName: "abc", birthDate: Date.now()}
+
+// Khai báo biến greetingUppercase và greetingReplaced với giá trị greeting 
+// được in hoa và thay thế "Hello" bằng "Good afternoon"
--- L2/js/bai2.js
+++ L2/js/bai2.js
@@ -1,23 +1,32 @@
-// Khai báo một biến String và thực hiện các thao tác như nối chuỗi, cắt chuỗi (substring), 
+// Khai báo một biến String và thực hiện các thao tác như nối chuỗi, cắt chuỗi (substring),
 // và tìm độ dài của chuỗi.
 // khai bao
 let fullName = "ABC";
 // noi chuoi
-fullName += "MNO";
+fullName += " MNO";
 // substring
-const fristName = fullName.slice(0, 3);
+const firstName = fullName.slice(0, 3);
 console.log(fullName);
-console.log(fristName);
+console.log(firstName);
 console.log(fullName.substring(0, 3));
 
 // Nhập vào một chuỗi từ người dùng, sau đó in ra chuỗi đó đã được in hoa.
-const input = prompt("Name");
+// input
+const input = prompt("Name: ");
+// alert(input.trim()? input.toUpperCase() : "Chua dien ten");
+alert(input.trim().toUpperCase() || "Chua dien ten");
 
-// Khai báo một biến chứa tên người dùng (String) và tuổi (Number), 
-// sau đó in ra thông báo dạnttdtg: "Tên bạn là X và bạn Y tuổi."
-let name = "tdt"
-let age = 14;
-// Tạo một chương trình nhận vào một câu từ người dùng, 
+// Khai báo một biến chứa tên người dùng (String) và tuổi (Number),
+// sau đó in ra thông báo dạng: "Tên bạn là X và bạn Y tuổi."
+let name = "Dang Hoang Long";
+let age1 = 14;
+const result = `Tên bạn là ${name} và bạn ${age1} tuổi.`; // template string
+console.log(result);
+
+// Tạo một chương trình nhận vào một câu từ người dùng,
 // đếm số ký tự trong câu và in ra độ dài của câu đó.
+const sentence = prompt("Viet cau");
+console.log("Do dai cau", sentence.length);
+console.log("Tu", sentence.split(" ").length);
 
 // Sử dụng hàm prompt("…”); cho phép người dùng nhập dữ liệu từ bàn phím.
